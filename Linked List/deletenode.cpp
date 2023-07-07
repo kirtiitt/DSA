abb6699fbda8bf1f2c261894f4f92a73c529be10
Kirti <137528591+kirtiitt@users.noreply.github.com>
2023-07-07 18:00:49 +0530

    comments

--- Linked List/deletenode.cpp
+++ Linked List/deletenode.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        //just copy the next node into given node
+        //TC: O(1), SC: O(1)
         ListNode *N=node->next;
         node->val= N->val;
         node->next= N->next;
