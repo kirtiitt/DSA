ea9d0d434b9ffac30c02ca79c6b3c9fc57f41d7d
Kirti <137528591+kirtiitt@users.noreply.github.com>
2023-07-07 17:51:24 +0530

    comments

--- Linked List/middle.cpp
+++ Linked List/middle.cpp
@@ -10,7 +10,11 @@
  */
 class Solution {
 public:
+    //brute force: count the length n, then move n/2
+    //TC: O(n)+ O(n/2), SC: O(1)
     ListNode* middleNode(ListNode* head) {
+        //f moves by 2, s moves by 1 and when f reaches NULL (even length) or last element (odd length) s is at middle
+        //TC: O(n/2), SC: O(1)
         ListNode *slow= head, *fast=head;
         while(fast && fast->next) {
             slow= slow->next;
