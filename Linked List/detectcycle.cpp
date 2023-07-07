c9339095e59cf687b3fa1b66fe8bca3856848f3d
Kirti <137528591+kirtiitt@users.noreply.github.com>
2023-07-07 18:44:46 +0530

    comments

--- Linked List/detectcycle.cpp
+++ Linked List/detectcycle.cpp
@@ -6,9 +6,15 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//brute force: using hash table, hash the address if address already present cycle otherwise no
+//TC: O(n), SC: O(n)
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        //optimized: f moves by 2 and s by 1, f catches up to s where cycle is present, if no cycle the f is NULL
+        //TC: O(n), SC:O(1)
         if(head==NULL)
             return 0;
         ListNode *s= head, *f= head;
