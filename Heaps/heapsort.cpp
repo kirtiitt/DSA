d3d16c0046de24e6b4dfbec3c0e2f69bd9a6ecef
Kirti <137528591+kirtiitt@users.noreply.github.com>
2023-07-15 02:51:01 +0530

    create

--- Heaps/heapsort.cpp
+++ Heaps/heapsort.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //TC for heapify: O(log n)
     void heapify(vector<int> &A, int size, int i) {
         int largest= i, left= 2*i+ 1, right= 2*i+ 2;
         if(left<size && A[left]> A[largest])
@@ -12,11 +13,13 @@ public:
         }
     }
 
+    //TC for build heap: O(n)
     void buildHeap(vector<int> &A, int size) {
         for(int i=(size/2-1); i>=0; i--)
             heapify(A, size, i);
     }
 
+    //TC: O(n* log n) for heap sort
     vector<int> sortArray(vector<int>& nums) {
         int size= nums.size();
         buildHeap(nums, size);
